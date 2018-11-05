3c589d53f0ec44069dc2001f412ba4e0ccdbfe77
wanglu <lwang057@163.com>
2018-11-05 22:46:33 +0800

    完善动画

--- app/src/main/java/com/lwang/customview/loadingview/LoadingView.java
+++ app/src/main/java/com/lwang/customview/loadingview/LoadingView.java
@@ -1,10 +1,16 @@
 package com.lwang.customview.loadingview;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.LinearLayout;
+
+import com.lwang.customview.R;
 
 /**
  * @author lwang
@@ -13,6 +19,14 @@ import android.widget.LinearLayout;
  */
 public class LoadingView extends LinearLayout {
 
+    private ShapeView mShapeView;
+    private View mShadowView;
+    private int mTranslationDistance = 0;
+    // 动画执行的时间
+    private final long ANIMATOR_DURATION = 350;
+    // 是否停止动画
+    private boolean mIsStopAnimator = false;
+
 
     public LoadingView(Context context) {
         super(context);
@@ -20,11 +34,74 @@ public class LoadingView extends LinearLayout {
 
     public LoadingView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        mTranslationDistance = dip2px(80);
         initLayout();
     }
 
     private void initLayout() {
+        // 直接加载布局
+        inflate(getContext(), R.layout.loading_view, this);
 
+        mShapeView = (ShapeView) findViewById(R.id.shape_view);
+        mShadowView = findViewById(R.id.shadow_view);
+
+    }
+
+    /**
+     * 开始下落动画
+     */
+    private void startFallAnimator() {
+
+    }
+
+    /**
+     * 开始执行上抛动画
+     */
+    private void startUpAnimator() {
+
+    }
+
+    /**
+     * 上抛的时候需要旋转
+     */
+    private void startRotationAnimator() {
+        ObjectAnimator rotationAnimator = null;
+        switch (mShapeView.getCurrentShape()) {
+            case Circle:
+            case Square:
+                // 旋转180
+                rotationAnimator = ObjectAnimator.ofFloat(mShapeView, "rotation", 0, 180);
+                break;
+            case Triangle:
+                // 旋转120
+                rotationAnimator = ObjectAnimator.ofFloat(mShapeView, "rotation", 0, -120);
+                break;
+            default:
+                break;
+        }
+        rotationAnimator.setDuration(ANIMATOR_DURATION);
+        rotationAnimator.setInterpolator(new DecelerateInterpolator());
+        rotationAnimator.start();
+    }
+
+    @Override
+    public void setVisibility(int visibility) {
+        // 不要再去排放和计算，少走一些系统的源码（View的绘制流程）
+        super.setVisibility(View.INVISIBLE);
+
+        // 清理动画
+        mShapeView.clearAnimation();
+        mShadowView.clearAnimation();
+
+        // 把LoadingView从父布局移除
+        ViewGroup parent = (ViewGroup) getParent();
+        if (parent != null) {
+            // 从父布局移除
+            parent.removeView(this);
+            // 移除自己所有的View
+            removeAllViews();
+        }
+        mIsStopAnimator = true;
     }
 
     private int dip2px(int dip) {
--- app/src/main/java/com/lwang/customview/loadingview/ShapeView.java
+++ app/src/main/java/com/lwang/customview/loadingview/ShapeView.java
@@ -3,19 +3,26 @@ package com.lwang.customview.loadingview;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.drawable.shapes.Shape;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.lwang.customview.R;
 
 /**
  * @author lwang
  * @date 2018/11/05
- * @description
+ * @description 自定义View（画圆、正方形、三角形）
  */
 public class ShapeView extends View {
 
-
+    // 默认开始画圆
+    private Shape mCurrentShape = Shape.Circle;
     private Paint mPaint;
+    private Path mPath;
 
     public ShapeView(Context context) {
         super(context);
@@ -31,12 +38,76 @@ public class ShapeView extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        // 只保证是正方形
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        setMeasuredDimension(Math.min(width, height), Math.min(width, height));
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        switch (mCurrentShape) {
+            case Circle:
+                // 画圆形
+                int center = getWidth() / 2;
+                mPaint.setColor(ContextCompat.getColor(getContext(), R.color.circle));
+                canvas.drawCircle(center, center, center, mPaint);
+                break;
+            case Square:
+                // 画正方形
+                mPaint.setColor(ContextCompat.getColor(getContext(), R.color.rect));
+                canvas.drawRect(0, 0, getWidth(), getHeight(), mPaint);
+                break;
+            case Triangle:
+                // 画三角  Path 画路线
+                mPaint.setColor(ContextCompat.getColor(getContext(), R.color.triangle));
+                if (mPath == null) {
+                    mPath = new Path();
 
+                }
+                canvas.drawPath(mPath, mPaint);
+                break;
+            default:
+                break;
+        }
     }
 
+    /**
+     * 不断重新绘制形状
+     */
+    public void changeShape() {
+        switch (mCurrentShape) {
+            case Circle:
+                mCurrentShape = Shape.Square;
+                break;
+            case Square:
+                mCurrentShape = Shape.Triangle;
+                break;
+            case Triangle:
+                mCurrentShape = Shape.Circle;
+                break;
+            default:
+                break;
+        }
+        invalidate();
+    }
+
+    /**
+     * 获取当前Shape
+     *
+     * @return
+     */
+    public Shape getCurrentShape() {
+        return mCurrentShape;
+    }
+
+    public enum Shape {
+        /**
+         * 圆，正方形，三角形
+         */
+        Circle,
+        Square,
+        Triangle
+    }
 
 }
