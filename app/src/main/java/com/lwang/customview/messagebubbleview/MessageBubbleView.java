075b2f9bbc0eef0f5b0fe942ffd926a82912ccb4
wanglu <lwang057@163.com>
2019-01-10 22:53:34 +0800

    增加消息拖拽view

--- app/src/main/java/com/lwang/customview/messagebubbleview/MessageBubbleView.java
+++ app/src/main/java/com/lwang/customview/messagebubbleview/MessageBubbleView.java
@@ -1,6 +1,7 @@
 package com.lwang.customview.messagebubbleview;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -19,5 +20,11 @@ public class MessageBubbleView extends View {
     public MessageBubbleView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
 
 }
