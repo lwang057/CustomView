c63886706b70380475678bd339e6d456fdc600f9
wanglu <lwang057@163.com>
2019-03-09 23:46:08 +0800

    完成贝塞尔qq气泡

--- app/src/main/java/com/lwang/customview/messagebubbleview/MessageBubbleView.java
+++ app/src/main/java/com/lwang/customview/messagebubbleview/MessageBubbleView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.PointF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -19,14 +20,15 @@ import android.view.View;
 public class MessageBubbleView extends View {
 
     //两个圆的圆形
-    private PointF mFixactionPoint, mDragPoint;
+    private PointF mDragPoint, mFixActionPoint;
     //拖拽圆的半径
     private int mDragRadius = 10;
     //画笔
     private Paint mPaint;
-    //固定圆的半径
-    private int mFixactionRadiusMax = 7;
-    private int mFixactionRadius;
+    //固定圆的最大半径（初始半径）
+    private int mFixActionRadiusMax = 7;
+    private int mFixActionRadiusMin = 3;
+    private int mFixActionRadius;
 
 
     public MessageBubbleView(Context context) {
@@ -36,8 +38,10 @@ public class MessageBubbleView extends View {
     public MessageBubbleView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         mDragRadius = dip2px(mDragRadius);
-        mFixactionRadiusMax = dip2px(mFixactionRadiusMax);
-        mPaint=new Paint();
+        mFixActionRadiusMax = dip2px(mFixActionRadiusMax);
+        mFixActionRadiusMin = dip2px(mFixActionRadiusMin);
+
+        mPaint = new Paint();
         mPaint.setColor(Color.RED);
         mPaint.setAntiAlias(true);
         mPaint.setDither(true);
@@ -46,17 +50,21 @@ public class MessageBubbleView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        if (mDragPoint == null || mFixactionPoint == null) {
+        if (mDragPoint == null || mFixActionPoint == null) {
             return;
         }
 
         //画拖拽圆
         canvas.drawCircle(mDragPoint.x, mDragPoint.y, mDragRadius, mPaint);
-        //画固定圆  有一个初始化大小，而且他的半径是随着距离的增大而减小  小到一定程度就不见了（不画了）
-        canvas.drawCircle(mDragPoint.x, mDragPoint.y, mDragRadius, mPaint);
 
+        Path beisaierPath = getBeisaierPath();
+        if (beisaierPath != null) {
+            //画固定圆  有一个初始化大小，而且他的半径是随着距离的增大而减小  小到一定程度就不见了（不画了）
+            canvas.drawCircle(mFixActionPoint.x, mFixActionPoint.y, mFixActionRadius, mPaint);
+            //画贝塞尔曲线
+            canvas.drawPath(beisaierPath, mPaint);
+        }
     }
-
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -81,6 +89,17 @@ public class MessageBubbleView extends View {
     }
 
     /**
+     * 初始化位置
+     *
+     * @param downX
+     * @param downY
+     */
+    private void initPoint(float downX, float downY) {
+        mFixActionPoint = new PointF(downX, downY);
+        mDragPoint = new PointF(downX, downY);
+    }
+
+    /**
      * 更新当前拖拽点的位置
      *
      * @param moveX
@@ -92,16 +111,85 @@ public class MessageBubbleView extends View {
     }
 
     /**
-     * 初始化位置
+     * 获取贝塞尔的路径
      *
-     * @param downX
-     * @param downY
+     * @return
      */
-    private void initPoint(float downX, float downY) {
-        mFixactionPoint = new PointF(downX, downY);
-        mDragPoint = new PointF(downX, downY);
+    private Path getBeisaierPath() {
+        //获取两个点的距离
+        double distance = getDistance(mDragPoint, mFixActionPoint);
+
+        //两点距离越大，固定圆的半径就越小
+        mFixActionRadius = (int) (mFixActionRadiusMax - distance / 14);
+        if (mFixActionRadius < mFixActionRadiusMin) {
+            //超过一定距离 贝塞尔和固定圆都不要画了
+            return null;
+        }
+
+        Path besaierPath = new Path();
+
+        // 求斜率
+        float dy = (mDragPoint.y - mFixActionPoint.y);
+        float dx = (mDragPoint.x - mFixActionPoint.x);
+        float tanA = dy / dx;
+
+        // 求角 a
+        double arcTanA = Math.atan(tanA);
+
+        // p0
+        float p0x = (float) (mFixActionPoint.x + mFixActionRadius * Math.sin(arcTanA));
+        float p0y = (float) (mFixActionPoint.y - mFixActionRadius * Math.cos(arcTanA));
+
+        // p1
+        float p1x = (float) (mDragPoint.x + mDragRadius * Math.sin(arcTanA));
+        float p1y = (float) (mDragPoint.y - mDragRadius * Math.cos(arcTanA));
+
+        // p2
+        float p2x = (float) (mDragPoint.x - mDragRadius * Math.sin(arcTanA));
+        float p2y = (float) (mDragPoint.y + mDragRadius * Math.cos(arcTanA));
+
+        // p3
+        float p3x = (float) (mFixActionPoint.x - mFixActionRadius * Math.sin(arcTanA));
+        float p3y = (float) (mFixActionPoint.y + mFixActionRadius * Math.cos(arcTanA));
+
+        // 控制点
+        PointF controlPoint = getControlPoint();
+
+        // 拼装贝塞尔的曲线路径（起始点与控制点）
+        // 从p0点开始画
+        besaierPath.moveTo(p0x, p0y);
+        // 画第一条  p0x, p0y为起点、  controlPoint.x, controlPoint.y为控制点、  p1x, p1y为终点
+        besaierPath.quadTo(controlPoint.x, controlPoint.y, p1x, p1y);
+
+        // 从p1连接到p2
+        besaierPath.lineTo(p2x, p2y);
+        // 画第二条  p2x, p2y为起点、  controlPoint.x, controlPoint.y为控制点、  p3x, p3y为终点
+        besaierPath.quadTo(controlPoint.x, controlPoint.y, p3x, p3y);
+
+        // 闭合
+        besaierPath.close();
+        return besaierPath;
     }
 
+    /**
+     * 获取两个圆之间的距离
+     *
+     * @param point1
+     * @param point2
+     * @return
+     */
+    private double getDistance(PointF point1, PointF point2) {
+        return Math.sqrt((point1.x - point2.x) * (point1.x - point2.x) + (point1.y - point2.y) * (point1.y - point2.y));
+    }
+
+    /**
+     * 获取控制点
+     *
+     * @return
+     */
+    private PointF getControlPoint() {
+        return new PointF((mDragPoint.x + mFixActionPoint.x) / 2, (mDragPoint.y + mFixActionPoint.y) / 2);
+    }
 
     private int dip2px(int dip) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dip, getResources().getDisplayMetrics());
