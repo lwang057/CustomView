7ea4287fb7e4f808db1dc7a2e2c890d8425a6ad6
lwang <lwang057@163.com>
2021-01-14 15:37:38 +0800

    更新代码

--- app/src/main/java/com/lwang/customview/MainActivity.java
+++ app/src/main/java/com/lwang/customview/MainActivity.java
@@ -6,8 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.lwang.customview.colortrackview.ColorTrackViewActivity;
-import com.lwang.customview.fingerprintview.FingerPrintActivity;
-import com.lwang.customview.floatview.FloatActivity;
 import com.lwang.customview.guideview.GuideActivity;
 import com.lwang.customview.indicatorseekbar.IndicatorSeekBarActivity;
 import com.lwang.customview.lettersideview.LetterSideViewActivity;
@@ -18,7 +16,6 @@ import com.lwang.customview.lovelayout.LoveLayoutActivity;
 import com.lwang.customview.messagebubbleview.MessageBubbleViewActivity;
 import com.lwang.customview.dragswipeview.DragSwipeListActivity;
 import com.lwang.customview.passwordedittext.PasswordEditTextActivity;
-import com.lwang.customview.qrcodeview.QRCodeViewActivity;
 import com.lwang.customview.slidingmenuview.SlidingMenuViewActivity;
 import com.lwang.customview.stepqqview.StepQQViewActivity;
 import com.lwang.customview.translationbehavior.TranslationBehaviorActivity;
@@ -46,8 +43,7 @@ public class MainActivity extends AppCompatActivity {
             R.id.button_letter_side_view, R.id.button_sliding_menu_view, R.id.button_indicator_seekbar,
             R.id.button_lock_pattern, R.id.button_translation_behavior, R.id.button_password_edittext,
             R.id.button_loading_view, R.id.button_loading_data_view, R.id.button_message_bubble_view,
-            R.id.button_guide_view, R.id.button_love_layout, R.id.button_finger_view, R.id.button_float_view,
-            R.id.button_drag_swipe, R.id.button_qr_code_view})
+            R.id.button_guide_view, R.id.button_love_layout, R.id.button_drag_swipe})
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button_wave_view:
@@ -92,17 +88,8 @@ public class MainActivity extends AppCompatActivity {
             case R.id.button_love_layout:
                 startActivity(new Intent(this, LoveLayoutActivity.class));
                 break;
-            case R.id.button_finger_view:
-                startActivity(new Intent(this, FingerPrintActivity.class));
-                break;
-            case R.id.button_float_view:
-                startActivity(new Intent(this, FloatActivity.class));
-                break;
             case R.id.button_drag_swipe:
                 startActivity(new Intent(this, DragSwipeListActivity.class));
-                break;
-            case R.id.button_qr_code_view:
-                startActivity(new Intent(this, QRCodeViewActivity.class));
                 break;
             default:
                 break;
